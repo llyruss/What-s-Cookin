cca2b5840c2bf6b991386b2af91c97e188ecc2c5
Lily Russ <llyruss@yahoo.com>
2022-12-10 11:09:09 -0600

    added home routes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,28 +1,42 @@
 const router = require('express').Router();
+const { User, Recipe, Ingredient, Direction } = require('../models');
+const withAuth = require('../utils/auth');
 
 router.get('/', async (req, res) => {
   // Send the rendered Handlebars.js template back as the response
   res.render('home');
 });
 
+router.get('/recipes', async (req, res) => {
+  try { 
+    const recipeData = await Recipe.findAll({
+        include: [
+          {
+              model: User,
+              attributes: ["name"]
+          },
+      ],
+  });
+  const recipes = recipeData.map((recipe) => recipe.get({ plain: true }));
+  res.render("get-all-recipes", { 
+    recipes,
+})
+} catch(err) {
+  console.log(err);
+  res.status(500).json(err)
+}
+});
+
+router.get('/login', (req, res) => {
+
+  if (req.session.logged_in) {
+    res.redirect('/recipes');
+    return;
+  }
+  res.render('login', {
+    logged_in: req.session.logged_in
+  });
+});
+
 module.exports = router;
 
-// router.get('/', async (req, res) => {
-//     try {
-//       // Get all projects and JOIN with user data
-      
-      
-  
-//       // Serialize data so the template can read it
-  
-//       // Pass serialized data and session flag into template
-//       res.render('get-all-recipes', { 
-        
-//       //   logged_in: req.session.logged_in 
-//       });
-//     } catch (err) {
-//       res.status(500).json(err);
-//     }
-//   });
-  
-//   module.exports = router;
